4d61d0d58d42edcf8ea4e028863e3dbee10873ee
Ксения Быстрова <ksenya_bystrova@mail.ru>
2020-07-22 14:14:02 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace PlaylistRandomizer
+{
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Директория с музыкальными файлами
+        /// </summary>
+        private string Path { get; set; }
+
+        /// <summary>
+        /// Музыкальные файлы
+        /// </summary>
+        private List<string> Songs { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Получение предыдущего выбранного расположения директории с музыкой
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            DirectoryInfo di = Directory.CreateDirectory(appDataPath + "\\PlaylistRandomizer");
+            StreamReader sr;
+            try
+            {
+                sr = new StreamReader(System.IO.Path.Combine(di.FullName, "path.txt"));
+                TbPath.Text =  Path = sr.ReadLine();
+                sr.Close();
+            }
+            catch
+            {
+                //Если первый запуск, то директория -- стандартная системная папка с музыкой пользователя
+                TbPath.Text = Path = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+                StreamWriter sw = new StreamWriter(System.IO.Path.Combine(di.FullName, "path.txt"), false);
+                sw.WriteLine(Path);
+                sw.Close();
+            }
+        }
+
+        /// <summary>
+        /// Обработчик нажатия на кнопку "Выбрать папку"
+        /// </summary>
+        private void BtnSelectPath_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog
+            {
+                SelectedPath = Path
+            };
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TbPath.Text = Path = fbd.SelectedPath;
+                StreamWriter sw = new StreamWriter(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + 
+                    "\\PlaylistRandomizer", "path.txt"), false);
+                sw.WriteLine(Path);
+                sw.Close();
+            }
+        }
+
+        /// <summary>
+        /// Обработчик нажатия на кнопку "Создать плейлист"
+        /// </summary>
+        private void BtnMix_Click(object sender, RoutedEventArgs e)
+        {
+            LbResult.Items.Clear();
+            BtnSave.Visibility = Visibility.Hidden;
+
+            if (!int.TryParse(TbAmount.Text, out int count))
+            {
+                System.Windows.MessageBox.Show("Указано неверное количество");
+                return;
+            }
+
+            Songs = Randomizer.GetRandomizedMP3s(Path, count);
+            if (Songs == null)
+            {
+                System.Windows.MessageBox.Show("В выбранной папке недостаточно mp3-файлов");
+                return;
+            }
+
+            foreach (string song in Songs) LbResult.Items.Add(song);
+            BtnSave.Visibility = Visibility.Visible;
+        }
+
+        /// <summary>
+        /// Обработчик нажатия на кнопку "Сохранить этот плейлист"
+        /// </summary>
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (Songs == null) return;
+
+            string playlist = ConfigurationManager.AppSettings["playlist"] + "\n";
+            foreach (string song in Songs) playlist += "\n" + song;
+
+            Stream stream;
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "AIMP playlist (*.aimppl4)|*.aimppl4",
+                RestoreDirectory = true
+            };
+
+            if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if ((stream = saveFileDialog.OpenFile()) != null)
+                {
+                    StreamWriter sw = new StreamWriter(stream, Encoding.BigEndianUnicode);
+                    sw.Write(playlist + "\n");
+                    sw.Close();
+                    stream.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Randomizer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PlaylistRandomizer
+{
+    public static class Randomizer
+    {
+        private static readonly Random RNG = new Random();
+
+        /// <summary>
+        /// Возвращает заданное количество рандомных mp3 из заданной директории
+        /// </summary>
+        /// <param name="defaultPath">Директория для поиска</param>
+        /// <param name="count">Количество требуемых mp3-файлов</param>
+        /// <returns>Список mp3 (либо null, если в директории не хватает mp3)</returns>
+        public static List<string> GetRandomizedMP3s(string defaultPath, int count)
+        {
+            //Проверяем, достаточно ли в заданной директории mp3-файлов
+            DirectoryInfo di = new DirectoryInfo(defaultPath);
+            if (di.GetFiles("*.mp3", SearchOption.AllDirectories).Length < count) return null;
+
+            List<string> songs = new List<string>();
+            for (int i = 0; i < count; i++)
+            {
+                string song = null;
+                while (song == null) song = GetSong(defaultPath);
+
+                //Если данная песня уже есть в плейлисте -- пропускаем
+                if (songs.Contains(song))
+                {
+                    i--;
+                    continue;
+                }
+                else songs.Add(song);
+            }
+
+            return songs;
+        }
+
+        /// <summary>
+        /// Выбирает случайный mp3 файл из данной директории
+        /// </summary>
+        /// <param name="defaultPath">Директория для поиска</param>
+        /// <returns>Случайный mp3-файл (либо null, если в папке нет mp3-файлов)</returns>
+        private static string GetSong(string defaultPath)
+        {
+            DirectoryInfo di = new DirectoryInfo(defaultPath);
+            DirectoryInfo[] dirs = di.GetDirectories();
+            FileInfo[] mp3s = di.GetFiles("*.mp3");
+
+            //Если там, куда нас завело, нет ни других папок, ни песен -- выходим
+            if (dirs.Length == 0 && mp3s.Length == 0) return null;
+
+            if (dirs.Length != 0)
+            {
+                //Если в исходной директории есть другие папки, а также mp3-файлы
+                if (mp3s.Length != 0)
+                {
+                    //Выбираем, будем ли мы заходить в другие папки
+                    if (Convert.ToBoolean(RNG.Next(0, 2)))
+                    {
+                        return GetSong(dirs[RNG.Next(dirs.Length)].FullName);
+                    }
+                    //Если не будем -- выбираем из тех mp3, что есть в исходной директории
+                    else return mp3s[RNG.Next(mp3s.Length)].FullName;
+                }
+                //Если других mp3 нет -- идём в подпапки
+                else return GetSong(dirs[RNG.Next(dirs.Length)].FullName);
+            }
+            //Если в исходной директории нет подпапок, но есть mp3 -- выбираем из них
+            else return mp3s[RNG.Next(mp3s.Length)].FullName;
+        }
+    }
+}
